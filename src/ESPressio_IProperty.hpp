68c903196dd4769d7c8db103de48f00175971a30
LK-Simon <lakraven@gmail.com>
2024-02-27 14:53:41 +0100

    - Introduced `ITypedProperty` to simplify Class Member Definitions and Usage

--- src/ESPressio_IProperty.hpp
+++ src/ESPressio_IProperty.hpp
@@ -3,6 +3,7 @@
 #include <ArduinoJson.h>
 #include <cstdlib>
 #include <cstring>
+#include <functional>
 
 namespace ESPressio {
 
@@ -21,6 +22,27 @@ namespace ESPressio {
                 virtual void ReadValueFromJson(JsonDocument& jsonDocument, bool useDefaultValue = false) = 0;
                 virtual void ResetToDefault() = 0;
                 virtual const char* GetName() = 0;
+        };
+
+        template <typename T>
+        class ITypedProperty : public IProperty {
+            public:
+                using TOnValueChanged = std::function<void(T oldValue, T newValue)>;
+                
+            // Getters
+                
+                virtual T GetValue() = 0;
+                virtual T GetDefaultValue() = 0;
+                virtual TOnValueChanged GetOnValueChanged() = 0;
+                virtual TOnValueChanged GetOnDefaultValueChanged() = 0;
+
+            // Setters
+
+                virtual void SetValue(T value) = 0;
+                virtual void SetDefaultValue(T defaultValue) = 0;
+                virtual void SetOnValueChanged(TOnValueChanged onValueChanged) = 0;
+                virtual void SetOnDefaultValueChanged(TOnValueChanged onDefaultValueChanged) = 0;
+
         };
 
     }
