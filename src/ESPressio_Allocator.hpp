dfcf0651e3099495ca56d9a0f7c66d36177860a3
Simon J Stuart <lakraven@gmail.com>
2026-08-25 10:19:27 +0200

    feat(#25): add opt-in prefer-PSRAM serialization policy

--- src/ESPressio_Allocator.hpp
+++ src/ESPressio_Allocator.hpp
@@ -1,8 +1,27 @@
 #pragma once
+
 #include <cstddef>
 #include <memory>
 #include <vector>
-#ifndef ESPRESSIO_SERIALIZATION_ALLOCATOR
-#define ESPRESSIO_SERIALIZATION_ALLOCATOR std::allocator
+
+#if defined(ESP32) && defined(ESPRESSIO_SERIALIZATION_PREFER_PSRAM)
+    #include "ESPressio_Esp32Allocator.hpp"
 #endif
-namespace ESPressio::Serializable { template<typename T> using SerializationAllocator = ESPRESSIO_SERIALIZATION_ALLOCATOR<T>; template<typename T> using SerializationBuffer = std::vector<T,SerializationAllocator<T>>; }
+
+#ifndef ESPRESSIO_SERIALIZATION_ALLOCATOR
+    #if defined(ESP32) && defined(ESPRESSIO_SERIALIZATION_PREFER_PSRAM)
+        #define ESPRESSIO_SERIALIZATION_ALLOCATOR ESPressio::Serializable::Esp32PreferPsramAllocator
+    #else
+        #define ESPRESSIO_SERIALIZATION_ALLOCATOR std::allocator
+    #endif
+#endif
+
+namespace ESPressio::Serializable {
+
+template<typename T>
+using SerializationAllocator = ESPRESSIO_SERIALIZATION_ALLOCATOR<T>;
+
+template<typename T>
+using SerializationBuffer = std::vector<T, SerializationAllocator<T>>;
+
+}
